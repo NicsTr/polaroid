88a6c9b440c0d72121408a0d93f04ad393ef8b53
_Frky <saouleur@gmail.com>
2017-12-22 18:26:21 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,3 @@ cd website/static/polaroid/ && compass compile
 ./manage.py collectstatic --noinput -c
 
 ./manage.py migrate
-
-uwsgi --module polaroid.wsgi \
-      --socket eloquence.sock \
-      --chmod-socket=776
